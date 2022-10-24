4b9fa1e351c2f7c2faf5799b196b7112aca74235
ConnorMcGehee <connorjames050@gmail.com>
2022-10-23 22:25:37 -0500

    Finished even/odd list

---
+++ scripts.js
@@ -0,0 +1,25 @@
+let usersWord = ""
+let evenList = document.getElementById("even")
+let oddList = document.getElementById("odd")
+
+
+function updateWord(element) {
+    usersWord = element.value
+}
+
+function handleSubmit() {
+    let newListItem = document.createElement('LI')
+    newListItem.innerText = usersWord
+
+    /* compare remainder to 0 to see if even or not */
+    if (usersWord.length % 2 === 0) {
+        /*put word in even lists*/
+        evenList.appendChild(newListItem)
+    } else {
+        /*put word in odd list*/
+        oddList.appendChild(newListItem)
+    }
+
+    usersWord = ""
+    document.getElementById("even-odd-form").reset()
+}
